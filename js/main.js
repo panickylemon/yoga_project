14a87460fe4a95b453fba4e3b75b373adf46d2b7
Alexey <zolotukhin89@gmail.com>
2016-08-10 01:29:35 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -4,12 +4,21 @@ $(document).ready(function(){
         $('.pointer-menu').removeClass('hidden-arrow');
         $('.menu-place a').addClass('blue-menu');
         $('.submenu').css("margin-top", "-17px");
+        $('.menu-place a span').css("border-bottom", "1px solid #0085b2");
     });
     $(".submenu-place, .menu-place").mouseleave(function(){
         $('.submenu-place').addClass('hidden-submenu-place');
         $('.pointer-menu').addClass('hidden-arrow');
         $('.menu-place a').removeClass('blue-menu');
         $('.submenu').css("margin-top", "0px");
+        $('.menu-place a span').css("border-bottom", "1px solid #999999");
+    });
+
+
+    $('.collapse').on('shown.bs.collapse', function(){
+        $(this).parent().find(".accordion-toggle-collapsed").removeClass("accordion-toggle-collapsed").addClass("accordion-toggle-expanded");
+    }).on('hidden.bs.collapse', function(){
+        $(this).parent().find(".accordion-toggle-expanded").removeClass("accordion-toggle-expanded").addClass("accordion-toggle-collapsed");
     });
 
 
