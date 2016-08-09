2ef3d8e7da74df7ad6dcf5e6bb1f0e2cb13047d0
panickylemon <alenka__1992@mail.ru>
2016-08-09 17:35:38 +0300

    menu mobile start

--- js/main.js
+++ js/main.js
@@ -3,10 +3,12 @@ $(document).ready(function(){
         $('.submenu-place').removeClass('hidden-submenu-place');
         $('.pointer-menu').removeClass('hidden-arrow');
         $('.menu-place a').addClass('blue-menu');
+        $('.submenu').css("margin-top", "-17px");
     });
     $(".submenu-place, .menu-place").mouseleave(function(){
         $('.submenu-place').addClass('hidden-submenu-place');
         $('.pointer-menu').addClass('hidden-arrow');
         $('.menu-place a').removeClass('blue-menu');
+        $('.submenu').css("margin-top", "0px");
     });
 });
